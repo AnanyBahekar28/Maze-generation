52f6c2daf9f520b8b64f217a5da9c3249c7c47ca
AnanyBahekar28 <63043517+AnanyBahekar28@users.noreply.github.com>
2021-04-29 15:14:17 +0530

    Pseudo code - Approach 2

---
+++ Pseudo code - Approach 2.py
@@ -0,0 +1,23 @@
+"""
+Configure the start cell
+Configure bot cell          #  bot cell will change according to the movement towards the target cell
+Configure the target cell
+def createMaze()):
+    def createPath():
+        the path will randomly generated with the random choosing of ways for the bot
+        
+    def createOtherPartOfMaze():
+        Randomly generate the blocks
+    
+    createPath()
+    createOtherPartOfMaze()
+def findPath():
+    loop until the bot has found a path using the path finding breadth first code.
+        if bot cell == target cell:
+            break
+        else:
+            continue finding path
+if __name__ == "__main__":
+    createMaze()
+    findPath()
+"""
